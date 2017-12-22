6c7793a26ee92f3aaf763ed7b9c60ed2ee1f7ad2
Adrián Gómez <adgllorente@gmail.com>
2017-12-22 13:44:30 +0100

    [[FIX]] totalNotifications was not obtained ok

--- webview.js
+++ webview.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = Franz => {
   const getMessages = () => {
-    const totalNotifications = document.querySelectorAll('#notificationList > .notification');
+    const totalNotifications = document.querySelectorAll('#notificationList > .notification').length;
     const hasUnread = document.querySelectorAll('#notificationsButton.hasUnread').length > 0;
 
     // set Franz badge
